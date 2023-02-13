f2e55396675f6332ab82d13aa736f345b44fc153
unknown <ignaciopb.ip@gmail.com>
2023-02-14 00:49:17 +0100

    subida del adquisition

---
+++ adquisicion-data.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import pandas as pd
+import datetime
+import time as t
+import codecs
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+
+
+
+
+URL = 'https://url-api'
+resp = requests.get(URL)
+resptext = resp.text
+
+print(resptext)
+respuesta = json.loads(resptext)
+df = pd.read_json(respuesta)
+log = str(resp.status_code)
+fichero = 'fichero.csv'
+df.to_csv(fichero, sep='|', index=False)
+
